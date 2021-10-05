7f42fd705868bdcd3727a0efded65987ab5d5ad4
Gilson Gama <you@example.com>
2021-10-05 04:53:32 -0300

    Alteração dos dados dos Contatos

---
+++ AlteraContatos.py
@@ -0,0 +1,25 @@
+import sqlite3
+conector = sqlite3.connect("agenda.db")
+cursor = conector.cursor()
+sql = """update contatos set Cel = ?, Tel = ?
+where NumContato = ?"""
+
+print("Digite os dados separados por vírgulas")
+print("Celular, Telefone, Número do Contato")
+
+Ler = input()
+while Ler != "":
+    D = Ler.split(",")
+    try:
+        cursor.execute(sql, D)
+        conector.commit()
+    except:
+        print("{} Dados inválidos".format(D))
+    else:
+        print(" "*50, "...dados alterados com sucesso")
+    finally:
+        print("Celular, Telefone, Número do Contato")
+        Ler = input()
+cursor.close()
+conector.close()
+
